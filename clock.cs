5bdbe1497114c0bd5dbbe968cb7cb65e16df7a36
CordeliusVox <cordeliusvox@gmail.com>
2024-12-25 10:24:53 +0200

    Update clock.cs

--- clock.cs
+++ clock.cs
@@ -7,6 +7,9 @@ class Program
     {
         Console.CursorVisible = false;
 
+        // Variable to hold the current color
+        ConsoleColor currentColor = ConsoleColor.Green;
+
         while (true)
         {
             Console.Clear();
@@ -14,14 +17,17 @@ class Program
             // Get current time
             DateTime now = DateTime.Now;
 
-            // Draw the clock
-            DrawBigClock(now);
+            // Draw the clock with the current color
+            DrawBigClock(now, currentColor);
+
+            // Change the color for the next second
+            currentColor = GetNextColor(currentColor);
 
             Thread.Sleep(1000); // Update every second
         }
     }
 
-    static void DrawBigClock(DateTime time)
+    static void DrawBigClock(DateTime time, ConsoleColor color)
     {
         string hour = time.Hour.ToString("D2");
         string minute = time.Minute.ToString("D2");
@@ -39,23 +45,35 @@ class Program
         {
             char c = currentTime[i];
             int index = c == ':' ? 10 : c - '0'; // Handle ':' as the 10th index
-            DrawBigCharacter(bigNumbers[index], startX + (i * 8), startY);
+            DrawBigCharacter(bigNumbers[index], startX + (i * 8), startY, color);
         }
     }
 
-    static void DrawBigCharacter(string[] bigChar, int startX, int startY)
+    static void DrawBigCharacter(string[] bigChar, int startX, int startY, ConsoleColor color)
     {
-        Random random = new Random();
-        ConsoleColor color = (ConsoleColor)random.Next(1, 16);
+        Console.ForegroundColor = color;
 
         for (int y = 0; y < bigChar.Length; y++)
         {
             Console.SetCursorPosition(startX, startY + y);
-            Console.ForegroundColor = color;
             Console.Write(bigChar[y]);
         }
 
         Console.ResetColor();
+    }
+
+    static ConsoleColor GetNextColor(ConsoleColor currentColor)
+    {
+        // Cycle through a predefined set of colors
+        ConsoleColor[] colors = 
+        {
+            ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Blue,
+            ConsoleColor.Yellow, ConsoleColor.Cyan, ConsoleColor.Magenta,
+            ConsoleColor.White
+        };
+
+        int nextIndex = (Array.IndexOf(colors, currentColor) + 1) % colors.Length;
+        return colors[nextIndex];
     }
 
     static string[][] GetBigNumbers()
