3cc1f851aad5c23f1b7460e46df5a0d939a94a24
CordeliusVox <cordeliusvox@gmail.com>
2025-01-05 12:28:33 +0200

    Create Ragdoll.lua

---
+++ Ragdoll.lua
@@ -0,0 +1,9 @@
+local Table = {
+  ["Left Arm"] = {CFrame = 0.5, 0.5, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0}
+  ["Left Leg"] = {CFrame = }
+  ["Right Arm"] = {CFrame = }
+  ["Right Leg"] = {CFrame = }
+  ["Head"] = {CFrame = 0, -0.5, 0, -1, 0, 0, 0, 1, 0, 0, 0, -1}
+  ["HumanoidRootPart"] = {CFrame = 0, 0, 0, -1, 0, 0, 0, 1, 0, 0, 0, -1} -- x2
+  ["Torso"] = {CFrame = }
+}
