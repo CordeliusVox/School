27c78759f874137e0c955e46033d0099f6f34500
CordeliusVox <cordeliusvox@gmail.com>
2025-01-08 08:42:22 +0200

    Update Ragdoll.lua

--- Ragdoll.lua
+++ Ragdoll.lua
@@ -1,9 +1,10 @@
-local Table = {
-  ["Left Arm"] = {CFrame = 0.5, 0.5, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0}
-  ["Left Leg"] = {CFrame = }
-  ["Right Arm"] = {CFrame = }
-  ["Right Leg"] = {CFrame = }
-  ["Head"] = {CFrame = 0, -0.5, 0, -1, 0, 0, 0, 1, 0, 0, 0, -1}
-  ["HumanoidRootPart"] = {CFrame = 0, 0, 0, -1, 0, 0, 0, 1, 0, 0, 0, -1} -- x2
-  ["Torso"] = {CFrame = }
-}
+local RagdollService = {}
+
+function RagdollService:ModifyMotor6D(Character: Model, Enabled: boolean): ()
+    for _, Motor in pairs(Character:GetDescendants()) do
+        if Motor:IsA("Motor6D") then
+            Motor.Enabled = Enabled
+        end
+    end
+end
+return RagdollService
