ea79ce827b2845e6ead8625036e19e52d94e5e8a
CordeliusVox <cordeliusvox@gmail.com>
2025-01-08 09:12:21 +0200

    Update Ragdoll.lua

--- Ragdoll.lua
+++ Ragdoll.lua
@@ -1,39 +1,46 @@
 local RagdollService = {}
 
 function RagdollService:RagdollCharacter(Character: Model, Enabled: boolean): ()
-    for _, Motor in pairs(Character:GetDescendants()) do
-        if Motor:IsA("Motor6D") then
-            local Part0 = Motor.Part0
-            local Part1 = Motor.Part1
+	for _, Motor in pairs(Character:GetDescendants()) do
+		if Motor:IsA("Motor6D") then
+			local Part0 = Motor.Part0
+			local Part1 = Motor.Part1
 
-            if Part0 and Part1 the
-                local C0 = Motor.C0
-                local C1 = Motor.C1
+			if Part0 and Part1 then
+				local C0 = Motor.C0
+				local C1 = Motor.C1
 
-                Motor.Enabled = Enabled
-                self:CreateAttachments(Part0, Part1, C0, C1)
-            end
-        end
-    end
+				Motor.Enabled = Enabled
+				self:CreateAttachments(Part0, Part1, C0, C1)
+				
+				local Humanoid: Humanoid = Character:FindFirstChildOfClass("Humanoid")
+				if Humanoid then
+					Humanoid.RequiresNeck = false
+					Humanoid.BreakJointsOnDeath = false
+					Humanoid:ChangeState(Enum.HumanoidStateType.Physics)
+				end
+			end
+		end
+	end
 end
 
 function RagdollService:CreateAttachments(Part0, Part1, C0, C1)
-    local Attachment0 = instance.new("Attachment")
-    Attachment0.CFrame = C0
-    Attachment0.Parent = Part0
-    
-    local Attachment1 = instance.new("Attachment")
-    Attachment1.CFrame = C1
-    Attachment1.Parent = Part1
-    
-    local BallSocketConstraint = instance.new("BallSocketConstraint")
-    BallSocketConstraint.Attachment0 = Attachment0
-    BallSocketConstraint.Attachment1 = Attachment1
-    BallSocketConstraint.Parent = Part0
+	local Attachment0 = Instance.new("Attachment")
+	Attachment0.CFrame = C0
+	Attachment0.Parent = Part0
 
-    -- Debug
-    Attachment0.Visible = true
-    Attachment1.Visible = true
+	local Attachment1 = Instance.new("Attachment")
+	Attachment1.CFrame = C1
+	Attachment1.Parent = Part1
+
+	local BallSocketConstraint = Instance.new("BallSocketConstraint")
+	BallSocketConstraint.Attachment0 = Attachment0
+	BallSocketConstraint.Attachment1 = Attachment1
+	BallSocketConstraint.Parent = Part0
+
+	-- Debug
+	Attachment0.Visible = true
+	Attachment1.Visible = true
 end
 
 return RagdollService
