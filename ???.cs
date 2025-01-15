6290b654e7d944af2a208063372861b6d193bb04
CordeliusVox <cordeliusvox@gmail.com>
2025-01-15 09:30:06 +0200

    Create ???.cs

---
+++ ???.cs
@@ -0,0 +1,17 @@
+<Window x:Class="Checkers.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:Checkers"
+        mc:Ignorable="d"
+        Title="Checkers" 
+        WindowStyle="SingleBorderWindow"
+        MaxHeight="900" MinHeight="900" Height="900"
+        MaxWidth="1000" MinWidth="1000" Width="1000">
+    <Grid>
+        <StackPanel>
+            <Image Source="/Images/Wooden Background.jpg"/>
+        </StackPanel>
+    </Grid>
+</Window>
